6c996374e8c23a0b9c77046f5bb5d2b0b4ded2e7
psinghal <psinghal@test.com>
2023-06-02 13:55:15 +0000

    add main.go

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # session_cookie_auth_golang
+
+We will build an application with a /signin and a /welcome route.
+
+The /signin route will accept a users username and password, and set a session cookie if successful.
+The /welcome route will be a simple HTTP GET route which will show a personalized message to the currently logged in user.
+
+Will define a Refresh HTTP handler to renew the users session token every time they hit the /refresh route in our application
+
+/logout - to invlaidate the session and logs out the user
+
+# Testing
+Ensure that http webserver port is allowed on the firewall
+
+in POSTMAN or http client fire the query..
+POST http://10.145.70.97:8081/signin with body 
+{
+  "username": "user1",
+  "password": "user1hasthispassword1"
+}
+
+Response : 200 OK and Session Token will be returned
+Token ( Session cookie ) can be seen in console output like ...
+Session Token is  31602044-525c-41e6-97c2-4da64435b5f5
+
+For Welcome fire the query
+GET http://10.145.70.97:8081/welcome
+Response : Welcome user1!
+
+For Refresh
+POST http://10.145.70.97:8081/refresh
+Response: New Session Token is  31602044-525c-41e6-97c2-4da64435b5f5
+
+
+For logout
+GET http://10.145.70.97:8081/logout
+Calling the welcome and refresh routes after this will result in a 401 error.
--- main.go
+++ main.go
@@ -1,5 +1,198 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/google/uuid"
+)
+
+// Will store the users information as an in-memory map in our code, IN production this will be a database
+var users = map[string]string{
+	"user1": "user1hasthispassword1",
+	"user2": "user2hasthispassword2",
+}
+
+// this map stores the users sessions. For larger scale applications, you can use a database or cache for this purpose
+var sessions = map[string]session{}
+
+// each session contains the username of the user and the time at which it expires
+type session struct {
+	username string
+	expiry   time.Time
+}
+
+// we'll use this method later to determine if the session has expired
+func (s session) isExpired() bool {
+	return s.expiry.Before(time.Now()) // if before then True else False
+}
+
+// Create a struct that models the structure of a user in the request body
+// These values will come from client but will read in this format struct
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+func Signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		// If the structure of the body is wrong, return an HTTP error
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println("Bad request")
+		return
+	}
+	// Get the expected password from our in memory map
+	expectedPassword, ok := users[creds.Username] // returns password for key=creds.Username
+	// If a password exists for the given user
+	// AND, if it is the same as the password we received, the we can move ahead
+	// if NOT, then we return an "Unauthorized" status
+	if !ok || expectedPassword != creds.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Println("expected password not same as received")
+		return
+	}
+	// Create a new random session token
+	// we use the "github.com/google/uuid" library to generate UUIDs
+	sessionToken := uuid.NewString()
+	fmt.Println("Session Token is ", sessionToken)
+	expiresAt := time.Now().Add(120 * time.Second)
+
+	// Set the token in the session map, along with the session information
+	// Save in our inmemory Map for now , but usually its in cache & database
+	sessions[sessionToken] = session{
+		username: creds.Username,
+		expiry:   expiresAt,
+	}
+
+	// Finally, we set the client cookie for "session_token" as the session token we just generated
+	// we also set an expiry time of 120 seconds
+	// we write it back and send back to HTTP response as cookie
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   sessionToken,
+		Expires: expiresAt,
+	})
+
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	// We can obtain the session token from the requests cookies, which come with every request
+	c, err := r.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie { // http has ErrNoCookie error method inbuilt
+			// If the cookie is not set, return an unauthorized status
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		// For any other type of error, return a bad request status
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	sessionToken := c.Value
+
+	// We then get the session from our session map
+	userSession, exists := sessions[sessionToken]
+	if !exists {
+		// If the session token is not present in session map, return an unauthorized error
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// If the session is present, but has expired, we can delete the session, and return
+	// an unauthorized status
+	if userSession.isExpired() {
+		delete(sessions, sessionToken) // delete is inbuilt method to delete value from map searched by key
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// If the session is valid, return the welcome message to the user
+	w.Write([]byte(fmt.Sprintf("Welcome %s!", userSession.username)))
+}
+
+func Refresh(w http.ResponseWriter, r *http.Request) {
+	// (BEGIN) The code from this point is the same as the first part of the `Welcome` route
+	c, err := r.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	sessionToken := c.Value
+
+	userSession, exists := sessions[sessionToken]
+	if !exists {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if userSession.isExpired() {
+		delete(sessions, sessionToken)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// (END) The code until this point is the same as the first part of the `Welcome` route
+
+	// If the previous session is valid, create a new session token for the current user
+	newSessionToken := uuid.NewString()
+	expiresAt := time.Now().Add(120 * time.Second)
+
+	// Set the token in the session map, along with the user whom it represents
+	sessions[newSessionToken] = session{
+		username: userSession.username,
+		expiry:   expiresAt,
+	}
+
+	// Delete the older session token
+	delete(sessions, sessionToken)
+
+	// Set the new token as the users `session_token` cookie
+	fmt.Println("New Session Token is ", sessionToken)
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   newSessionToken,
+		Expires: time.Now().Add(120 * time.Second),
+	})
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			// If the cookie is not set, return an unauthorized status
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		// For any other type of error, return a bad request status
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	sessionToken := c.Value
+
+	// remove the users session from the session map
+	delete(sessions, sessionToken)
+
+	// We need to let the client know that the cookie is expired
+	// In the response, we set the session token to an empty
+	// value and set its expiry as the current time
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   "",
+		Expires: time.Now(),
+	})
+}
+
+func main() {
+	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/welcome", Welcome)
+	http.HandleFunc("/refresh", Refresh)
+	http.HandleFunc("/logout", Logout)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
